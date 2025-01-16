a731850f52f52bc4d8c5d00cfc51fe2704f81295
MD TARIQUL ISLAM <tariqul.developer@gmail.com>
2025-01-16 07:27:05 +0600

    get all donation data done

--- src/Components/DonationsCampaigns/DonationCard.jsx
+++ src/Components/DonationsCampaigns/DonationCard.jsx
@@ -1,36 +1,37 @@
 import PropTypes from "prop-types";
-import { FaGetPocket } from "react-icons/fa";
+import { AiFillDollarCircle } from "react-icons/ai";
 import { FaLocationDot } from "react-icons/fa6";
 import { MdAddReaction } from "react-icons/md";
 import { Link } from "react-router-dom";
 
-const PetCard = ({ pet = {} }) => {
-    const { _id, petImage, petName, petAge, petLocation } = pet
+const DonationCard = ({ donation = {} }) => {
+    console.log(donation);
+    const { _id, donationImage, donationName, maxAmount, totalDonateAmount } = donation
 
     return (
         <div>
             <Link to={`/pet-details/${_id}`} className="block rounded-lg p-4 shadow-sm shadow-indigo-100">
                 <img
                     alt=""
-                    src={petImage}
+                    src={donationImage}
                     className="h-56 w-full rounded-md object-cover"
                 />
 
                 <div className="mt-3">
                     <dl>
                         <div>
-                            <dd className="font-medium">{petName}</dd>
+                            <dd className="font-medium">{donationName}</dd>
                         </div>
                     </dl>
 
                     <div className="mt-6 flex items-center gap-8 text-xs">
                         <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-                            <FaGetPocket size={14} />
+                            <AiFillDollarCircle size={14} />
 
                             <div className="mt-1.5 sm:mt-0">
-                                <p className="text-gray-500">Age</p>
+                                <p className="text-gray-500">Max Amount</p>
 
-                                <p className="font-medium"> {petAge?.value} {petAge?.unit}</p>
+                                <p className="font-medium"> $ {maxAmount}</p>
                             </div>
                         </div>
 
@@ -38,9 +39,11 @@ const PetCard = ({ pet = {} }) => {
                             <FaLocationDot size={14} />
 
                             <div className="mt-1.5 sm:mt-0">
-                                <p className="text-gray-500">Location</p>
+                                <p className="text-gray-500">Donated Amount</p>
 
-                                <p className="font-medium">{petLocation?.length < 10 ? petLocation : `${petLocation?.slice(0, 7)}...`}</p>
+                                <p className="font-medium">
+                                    $ {totalDonateAmount}
+                                </p>
                             </div>
                         </div>
 
@@ -60,8 +63,8 @@ const PetCard = ({ pet = {} }) => {
     );
 };
 
-PetCard.propTypes = {
-    pet: PropTypes.object.isRequired,
+DonationCard.propTypes = {
+    donation: PropTypes.object.isRequired,
 }
 
-export default PetCard;
+export default DonationCard;
--- src/Pages/DonationCampaigns/DonationCampaigns.jsx
+++ src/Pages/DonationCampaigns/DonationCampaigns.jsx
@@ -1,8 +1,76 @@
-const DonationCampaigns = () => {
-    return (
-        <div>
+import useAxiosPublic from "@/Hook/useAxiosPublic";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import { CgSpinnerTwo } from "react-icons/cg";
+import DonationCard from "@/Components/DonationsCampaigns/DonationCard";
 
-        </div>
+const DonationCampaigns = () => {
+    const [sort, setSort] = useState('');
+    const axiosPublic = useAxiosPublic()
+    const { ref, inView } = useInView();
+    const { data: pets, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteQuery({
+        queryKey: ["petListingAllPet", sort],
+        queryFn: async ({ pageParam = 1 }) => {
+            const { data } = await axiosPublic.get("/donation-campaign", {
+                params: {
+                    sort,
+                    page: pageParam,
+                    limit: 6,
+                },
+            });
+            return data;
+        },
+        getNextPageParam: (lastPage, allPages) => {
+            return lastPage.length === 6 ? allPages.length + 1 : undefined;
+        },
+    });
+
+    useEffect(() => {
+        if (inView && hasNextPage) {
+            fetchNextPage()
+        }
+    }, [fetchNextPage, hasNextPage, inView]);
+
+    const handelSortChange = (e) => {
+        setSort(e.target.value)
+    }
+
+    return (
+        <div className="my-20">
+            <div className="w-11/12 mx-auto">
+                {/* Sort by date */}
+                <div className="mb-6 flex w-1/4">
+                    <select
+                        className="inputField p-2"
+                        value={sort}
+                        onChange={handelSortChange}
+                    >
+                        <option value="">Sort by date</option>
+                        <option value="new">New Date</option>
+                        <option value="old">Old Date</option>
+                    </select>
+                </div>
+
+                {/* Pet Cards */}
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {
+                        pets?.pages[0]?.map(donation => <DonationCard key={donation?._id} donation={donation} />)
+                    }
+                </div>
+            </div>
+
+            {/* Infinite Scroll Loader */}
+            <div ref={ref} className="h-4 mt-4">
+                {isFetchingNextPage && <CgSpinnerTwo color="#6b7280" size={20} />}
+            </div>
+
+            {!hasNextPage && pets?.pages.flat().length > 0 && (
+                <p className="text-center text-gray-500 mt-4">
+                    No more pets to load.
+                </p>
+            )}
+        </div >
     );
 };
 
