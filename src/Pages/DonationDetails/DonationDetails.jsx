1b3acb8443ff716cee2af79206f86a81bbf41e6f
MD TARIQUL ISLAM <tariqul.developer@gmail.com>
2025-01-17 19:20:33 +0600

    show 3 suggestion data on donation details page

--- src/Pages/DonationDetails/DonationDetails.jsx
+++ src/Pages/DonationDetails/DonationDetails.jsx
@@ -7,11 +7,12 @@ import useAxiosPublic from "@/Hook/useAxiosPublic";
 import DOMPurify from 'dompurify';
 import toast from 'react-hot-toast';
 import { useState } from 'react';
-import LoadingSpinner from '@/Components/Shared/LoadingSpinner';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import CheckoutForm from '../Payment/CheckoutForm';
 import { BiSolidDonateHeart } from 'react-icons/bi';
+import Skeleton from 'react-loading-skeleton';
+import DonationCard from '@/Components/DonationsCampaigns/DonationCard';
 
 const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_CLIENT_SECRET);
 
@@ -24,10 +25,20 @@ const DonationDetails = () => {
 
     const [isModalOpen, setIsModalOpen] = useState(false);
 
+    // Fetch donation details by ID
     const { data: donation = {}, isLoading, refetch } = useQuery({
         queryKey: ["donationDetails", id],
         queryFn: async () => {
             const { data } = await axiosPublic.get(`/donation-details/${id}`);
+            return data;
+        },
+    });
+
+    // Fetch suggested donation campaigns
+    const { data: suggestedDonation = [], isLoading: isLoadingSuggested } = useQuery({
+        queryKey: ["suggestionDonationCampaign"],
+        queryFn: async () => {
+            const { data } = await axiosPublic.get("/suggestion-donation-campaign");
             return data;
         },
     });
@@ -43,23 +54,29 @@ const DonationDetails = () => {
 
     const sanitizedDescription = DOMPurify.sanitize(donation?.longDescription);
 
-    if (isLoading) return <LoadingSpinner />
+    if (isLoading) return (
+        <div className="w-11/12 mx-auto flex flex-col lg:flex-row gap-8 my-16">
+            <div className="flex-1">
+                <Skeleton height={400} />
+            </div>
+
+            <div className="flex-1 space-y-4">
+                <Skeleton height={30} width="60%" />
+                <Skeleton height={20} width="40%" />
+                <Skeleton height={20} width="80%" />
+                <Skeleton height={20} width="50%" />
+                <Skeleton height={20} width="60%" />
+                <Skeleton height={30} width="80%" />
+                <Skeleton height={100} />
+            </div>
+        </div>
+    );
 
     return (
         <section>
             {/* Banner Section */}
             <div className="relative flex items-center h-[30vh] bg-cover bg-center"
-                style={{ backgroundImage: `url(https://img.freepik.com/premium-photo/silhouette-dog-looking-away-field-against-sky_1048944-17766562.jpg?w=740` }}>
-                <div className="relative w-11/12 text-center text-white z-10">
-                    <h1 className="font-bold text-5xl">
-                        Donation
-                        <span className="relative inline-block mx-2 text-white">
-                            <span className="absolute inset-0 bg-color-accent clip-trapezoid"></span>
-                            <span className="relative px-4 py-1">Details</span>
-                        </span>
-                    </h1>
-                </div>
-            </div>
+                style={{ backgroundImage: `url(https://img.freepik.com/premium-photo/silhouette-dog-looking-away-field-against-sky_1048944-17766562.jpg?w=740` }}></div>
 
             {/* donation Details Section */}
             <div className="donation-details w-11/12 mx-auto flex flex-col lg:flex-row gap-8 my-16">
@@ -81,8 +98,7 @@ const DonationDetails = () => {
                                 day: '2-digit',
                                 month: 'long',
                                 year: 'numeric'
-                            })}
-                            </p>
+                            })}</p>
                             <p>
                                 <strong>Status:</strong> <span className={`${donation?.status === 'Running' && 'text-green-600'} ${donation?.status === 'Pause' && 'text-red-600'} ${donation?.status === 'Complete' && 'text-blue-600 font-semibold'}`}>
                                     {donation?.status}
@@ -91,8 +107,7 @@ const DonationDetails = () => {
                         </div>
                         <div>
                             <CircleProgress progress={(donation?.totalDonateAmount / donation?.maxAmount) * 100 || 0}>
-                                <CircleProgressLine className="text-error-500" strokeColor="text-error-500"
-                                />
+                                <CircleProgressLine className="text-error-500" strokeColor="text-error-500" />
                                 <CircleProgressText>
                                     {Math.round((donation?.totalDonateAmount / donation?.maxAmount) * 100 || 0)}%
                                 </CircleProgressText>
@@ -122,7 +137,6 @@ const DonationDetails = () => {
                                         <ModalTitle>Donate Now</ModalTitle>
                                     </div>
                                     <div className="space-y-1 text-center">
-
                                         <ModalDescription>
                                             <CheckoutForm
                                                 donationId={donation?._id}
@@ -143,11 +157,30 @@ const DonationDetails = () => {
             </div>
 
             <div className="w-11/12 mx-auto my-20">
-                <p className="text-2xl text-color-accent-d font-semibold border-b-2 border-color-accent inline">Details More</p>
+                <h3 className="text-2xl text-color-accent-d font-semibold border-b-2 border-color-accent inline">Details More</h3>
                 <div
-                    className="donation-description text-base md:text-lg leading-relaxed mt-8"
+                    className="donation-description leading-relaxed mt-8"
                     dangerouslySetInnerHTML={{ __html: sanitizedDescription }}
                 />
+            </div>
+
+            <div className='w-11/12 mx-auto mb-20'>
+                <h3 className="text-2xl text-color-accent-d font-semibold border-b-2 border-color-accent inline">Suggestion</h3>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
+                    {isLoadingSuggested ? (
+                        Array(3).fill().map((_, index) => (
+                            <div key={index} className="p-4">
+                                <Skeleton height={200} />
+                                <Skeleton height={20} width="80%" className="mt-2" />
+                                <Skeleton height={20} width="60%" className="mt-2" />
+                            </div>
+                        ))
+                    ) : (
+                        suggestedDonation?.map((donation) => (
+                            <DonationCard key={donation?._id} donation={donation} />
+                        ))
+                    )}
+                </div>
             </div>
         </section>
     );
