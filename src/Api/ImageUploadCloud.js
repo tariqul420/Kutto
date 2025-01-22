bbe746d826e4ceab7ccfe688bd14da0aecd8c1af
MD TARIQUL ISLAM <tariqul.developer@gmail.com>
2025-01-22 17:23:52 +0600

    hide preset name

--- src/Api/ImageUploadCloud.js
+++ src/Api/ImageUploadCloud.js
@@ -5,10 +5,10 @@ export default async function ImageUploadCloud(imgData) {
 
     const formData = new FormData();
     formData.append('file', imgData);
-    formData.append('upload_preset', 'Kutto-Web');
+    formData.append('upload_preset', import.meta.env.VITE_CLOUDINARY_PRESET);
 
     try {
-        const res = await axios.post(`https://api.cloudinary.com/v1_1/dbjlihrjj/image/upload`, formData);
+        const res = await axios.post(`https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}/image/upload`, formData);
         return res.data.secure_url;
     } catch (err) {
         console.error("Error uploading image:", err);
