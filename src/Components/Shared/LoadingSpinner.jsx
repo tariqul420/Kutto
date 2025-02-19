9610a3780fdff04efc95bea2cb7075587d5ead72
tariqul420 <tariqul.developer@gmail.com>
2025-02-19 11:32:26 +0600

    all loading spainer full vh

--- src/Components/Shared/LoadingSpinner.jsx
+++ src/Components/Shared/LoadingSpinner.jsx
@@ -2,7 +2,7 @@ import { ScaleLoader } from 'react-spinners';
 
 const LoadingSpinner = () => {
     return (
-        <div className='flex items-center justify-center my-12'>
+        <div className='flex items-center justify-center min-h-screen'>
             <ScaleLoader
                 height={60}
                 margin={2}
